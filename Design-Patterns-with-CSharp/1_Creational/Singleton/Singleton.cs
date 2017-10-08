3e104edc85b87a777bff461933585da6be886585
iqans <iqan.shaikh@gmail.com>
2017-10-08 11:20:15 +0530

    singleton example done

--- Design-Patterns-with-CSharp/1_Creational/Singleton/Singleton.cs
+++ Design-Patterns-with-CSharp/1_Creational/Singleton/Singleton.cs
@@ -2,5 +2,33 @@
 {
     public class Singleton
     {
+        
+    }
+
+    public class MyFavoriteiriyani
+    {
+        private static MyFavoriteiriyani _biryani;
+        private static readonly object _syncObject = new object() { };
+
+        private MyFavoriteiriyani()
+        {
+        }
+
+        public static MyFavoriteiriyani Biriyani
+        {
+            get
+            {
+                if (_biryani == null)
+                {
+                    lock (_syncObject)
+                    {
+                        _biryani = new MyFavoriteiriyani();
+                    }
+                }
+                return _biryani;
+            }
+        }
+
+        public int BiriyaniPlates { get; set; }
     }
 }
--- Design-Patterns-with-CSharp/1_Creational_Tests/Singleton/SingletonTests.cs
+++ Design-Patterns-with-CSharp/1_Creational_Tests/Singleton/SingletonTests.cs
@@ -5,5 +5,19 @@ namespace _1_Creational.Singleton
     [TestFixture]
     public class SingletonTests
     {
+        [Test]
+        public void MyFavoriteBiriyani_Should_Always_Return_Same_Biriyani()
+        {
+            var myFavoriteBiriyani1 = MyFavoriteiriyani.Biriyani;
+            var myFavoriteBiriyani2 = MyFavoriteiriyani.Biriyani;
+
+            Assert.AreSame(myFavoriteBiriyani1, myFavoriteBiriyani2);
+
+            myFavoriteBiriyani1.BiriyaniPlates++;
+            Assert.AreEqual(myFavoriteBiriyani1.BiriyaniPlates, myFavoriteBiriyani2.BiriyaniPlates);
+
+            myFavoriteBiriyani2.BiriyaniPlates++;
+            Assert.AreEqual(myFavoriteBiriyani2.BiriyaniPlates, myFavoriteBiriyani1.BiriyaniPlates);
+        }
     }
 }
