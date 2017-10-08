438e5261cc3ea1f7205c1fb380dc882d8c20b0a1
iqans <iqan.shaikh@gmail.com>
2017-10-08 11:44:32 +0530

    prototype example done

--- Design-Patterns-with-CSharp/1_Creational/Prototype/Prototype.cs
+++ Design-Patterns-with-CSharp/1_Creational/Prototype/Prototype.cs
@@ -3,4 +3,59 @@
     public class Prototype
     {
     }
+
+    public abstract class BiriyaniPrototype
+    {
+        public bool IsSweet { get; set; }
+        public bool IsSpicy { get; set; }
+        public bool HasEdibleColor { get; set; }
+
+        public BiriyaniPrototype(bool isSweet, bool isSpicy, bool hasEdibleColor)
+        {
+            IsSweet = isSweet;
+            IsSpicy = isSpicy;
+            HasEdibleColor = hasEdibleColor;
+        }
+
+        public abstract BiriyaniPrototype Clone();
+    }
+
+    public class SweetBiriyani : BiriyaniPrototype
+    {
+        public SweetBiriyani(bool isSweet = true, bool isSpicy = false, bool hasEdibleColor = false) : 
+            base(isSweet, isSpicy, hasEdibleColor)
+        {
+        }
+
+        public override BiriyaniPrototype Clone()
+        {
+            return (SweetBiriyani) this.MemberwiseClone();
+        }
+    }
+
+    public class SpicyBiriyani : BiriyaniPrototype
+    {
+        public SpicyBiriyani(bool isSweet = false, bool isSpicy = true, bool hasEdibleColor = false) :
+            base(isSweet, isSpicy, hasEdibleColor)
+        {
+        }
+
+        public override BiriyaniPrototype Clone()
+        {
+            return (SpicyBiriyani)this.MemberwiseClone();
+        }
+    }
+
+    public class RamboBiriyani : BiriyaniPrototype
+    {
+        public RamboBiriyani(bool isSweet = false, bool isSpicy = false, bool hasEdibleColor = true) :
+            base(isSweet, isSpicy, hasEdibleColor)
+        {
+        }
+
+        public override BiriyaniPrototype Clone()
+        {
+            return (RamboBiriyani)this.MemberwiseClone();
+        }
+    }
 }
--- Design-Patterns-with-CSharp/1_Creational_Tests/Prototype/PrototypeTests.cs
+++ Design-Patterns-with-CSharp/1_Creational_Tests/Prototype/PrototypeTests.cs
@@ -5,5 +5,43 @@ namespace _1_Creational.Prototype
     [TestFixture]
     public class PrototypeTests
     {
+        [Test]
+        public void SweetBiryani_Should_Create_Another_SweetBiryani()
+        {
+            var sweetBiriyani = new SweetBiriyani();
+            var anotherBiriyani = sweetBiriyani.Clone();
+
+            Assert.AreEqual(sweetBiriyani.IsSweet, anotherBiriyani.IsSweet);
+            Assert.AreEqual(sweetBiriyani.IsSpicy, anotherBiriyani.IsSpicy);
+            Assert.AreEqual(sweetBiriyani.HasEdibleColor, anotherBiriyani.HasEdibleColor);
+
+            Assert.AreNotSame(sweetBiriyani, anotherBiriyani);
+        }
+
+        [Test]
+        public void SpicyBiryani_Should_Create_Another_SpicyBiryani()
+        {
+            var spicyBiriyani = new SpicyBiriyani();
+            var anotherBiriyani = spicyBiriyani.Clone();
+
+            Assert.AreEqual(spicyBiriyani.IsSweet, anotherBiriyani.IsSweet);
+            Assert.AreEqual(spicyBiriyani.IsSpicy, anotherBiriyani.IsSpicy);
+            Assert.AreEqual(spicyBiriyani.HasEdibleColor, anotherBiriyani.HasEdibleColor);
+
+            Assert.AreNotSame(spicyBiriyani, anotherBiriyani);
+        }
+
+        [Test]
+        public void RamboBiryani_Should_Create_Another_RamboBiryani()
+        {
+            var ramboBiriyani = new RamboBiriyani();
+            var anotherBiriyani = ramboBiriyani.Clone();
+
+            Assert.AreEqual(ramboBiriyani.IsSweet, anotherBiriyani.IsSweet);
+            Assert.AreEqual(ramboBiriyani.IsSpicy, anotherBiriyani.IsSpicy);
+            Assert.AreEqual(ramboBiriyani.HasEdibleColor, anotherBiriyani.HasEdibleColor);
+
+            Assert.AreNotSame(ramboBiriyani, anotherBiriyani);
+        }
     }
 }
