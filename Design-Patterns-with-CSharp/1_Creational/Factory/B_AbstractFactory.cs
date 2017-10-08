853565b1ffe1cfd096f11afbabd012610a13f622
iqans <iqan.shaikh@gmail.com>
2017-10-08 11:01:09 +0530

    abstract facctory example done

--- Design-Patterns-with-CSharp/1_Creational/Factory/B_AbstractFactory.cs
+++ Design-Patterns-with-CSharp/1_Creational/Factory/B_AbstractFactory.cs
@@ -1,6 +1,41 @@
-﻿namespace _1_Creational.Factory
+﻿using _1_Creational.Factory.Classes;
+
+namespace _1_Creational.Factory
 {
-    public class B_AbstractFactory
+    public static class B_AbstractFactory
     {
+
+    }
+
+    public abstract class BiriyaniFactory
+    {
+        public abstract IBiriyani CreateVegBiriyani();
+        public abstract IBiriyani CreateNonVegBiriyani();
+    }
+
+    public class SimpleBiriyaniFactory : BiriyaniFactory
+    {
+        public override IBiriyani CreateVegBiriyani()
+        {
+            return new VegBiriyani();
+        }
+
+        public override IBiriyani CreateNonVegBiriyani()
+        {
+            return new ChickenBiriyani();
+        }
+    }
+
+    public class SpecialBiriyaniFactory : BiriyaniFactory
+    {
+        public override IBiriyani CreateVegBiriyani()
+        {
+            return new VegBiriyani();
+        }
+
+        public override IBiriyani CreateNonVegBiriyani()
+        {
+            return new MuttonBiriyani();
+        }
     }
 }
--- Design-Patterns-with-CSharp/1_Creational_Tests/Factory/B_AbstractFactoryTests.cs
+++ Design-Patterns-with-CSharp/1_Creational_Tests/Factory/B_AbstractFactoryTests.cs
@@ -1,9 +1,45 @@
 ﻿using NUnit.Framework;
+using _1_Creational.Factory.Classes;
 
 namespace _1_Creational.Factory
 {
     [TestFixture]
     public class B_AbstractFactoryTests
     {
+        [Test]
+        public void SimpleBiryaniFactory_Should_Create_Veg_When_Asked_For_Veg_Biriyani()
+        {
+            var factory = new SimpleBiriyaniFactory();
+            var biriyani = factory.CreateVegBiriyani();
+
+            Assert.IsInstanceOf<VegBiriyani>(biriyani);
+        }
+
+        [Test]
+        public void SimpleBiryaniFactory_Should_Create_Chicken_When_Asked_For_NonVeg_Biriyani()
+        {
+            var factory = new SimpleBiriyaniFactory();
+            var biriyani = factory.CreateNonVegBiriyani();
+
+            Assert.IsInstanceOf<ChickenBiriyani>(biriyani);
+        }
+
+        [Test]
+        public void SpecialBiryaniFactory_Should_Create_Veg_When_Asked_For_Veg_Biriyani()
+        {
+            var factory = new SpecialBiriyaniFactory();
+            var biriyani = factory.CreateVegBiriyani();
+
+            Assert.IsInstanceOf<VegBiriyani>(biriyani);
+        }
+
+        [Test]
+        public void SpecialBiryaniFactory_Should_Create_Mutton_When_Asked_For_NonVeg_Biriyani()
+        {
+            var factory = new SpecialBiriyaniFactory();
+            var biriyani = factory.CreateNonVegBiriyani();
+
+            Assert.IsInstanceOf<MuttonBiriyani>(biriyani);
+        }
     }
 }
